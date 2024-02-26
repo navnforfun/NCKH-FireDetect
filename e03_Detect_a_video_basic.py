84209b8a47a65977a0fd403a0a9df13300f13e13
unknown <useforfun201@gmail.com>
2024-02-26 12:14:02 +0700

    Run model v8

--- e03_Detect_a_video_basic.py
+++ e03_Detect_a_video_basic.py
@@ -9,11 +9,11 @@ from ultralytics import  YOLO
 import  cv2
 import supervision as sv
 import numpy as np
-model  = YOLO('./model_v7.pt')
-VIDEO_PATH = r"C:\Users\Administrator\Documents\python\NCKH_FireDetect\Data\fire6.mp4"
+model  = YOLO('./model_v8.pt')
+VIDEO_PATH = r"C:\Users\Administrator\Documents\python\NCKH_FireDetect\Data\fire8.mp4"
 SAVE_FILE = r"runs\results\fire6_v3.mp4"
 # old
-# results = model.predict(r'C:\Users\Administrator\Documents\python\NCKH_FireDetect\Data\fire6.mp4',show=True,save = True, save_txt = True)
+results = model.predict(r'C:\Users\Administrator\Documents\python\NCKH_FireDetect\Data\fire8.mp4',show=True,save = True, save_txt = True)
 # # model.predict()b  
 # # print(results[0].tojson())
 # for r in results:
@@ -37,21 +37,21 @@ SAVE_FILE = r"runs\results\fire6_v3.mp4"
 # sv.process_video(source_path=VIDEO_PATH, target_path=SAVE_FILE, callback=process_frame)
 
 
-box_annotator = sv.BoxAnnotator(thickness=1, text_thickness=1, text_scale=1)
-for result in  model.track(source=VIDEO_PATH,stream=True,save=True,show=True):
-    frame = result.orig_img
-    detections = sv.Detections.from_yolov8(result)
-    detections = detections[detections.class_id != 0]
-    if result.boxes.id is not None:
-        detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
-    labels = [
-        f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
-        for _,confidence,class_id,tracker_id
-        in detections
-    ]
-    frame = box_annotator.annotate(scene=frame, detections=detections,labels=labels)
-    # cv2.imshow("yolov8",frame)
-    if(cv2.waitKey(30)==27):
-        cv2.destroyAllWindows()
-        break
+# box_annotator = sv.BoxAnnotator(thickness=1, text_thickness=1, text_scale=1)
+# for result in  model.track(source=VIDEO_PATH,stream=True,save=True,show=True):
+#     frame = result.orig_img
+#     detections = sv.Detections.from_yolov8(result)
+#     detections = detections[detections.class_id != 0]
+#     if result.boxes.id is not None:
+#         detections.tracker_id = result.boxes.id.cpu().numpy().astype(int)
+#     labels = [
+#         f"{tracker_id} {model.model.names[class_id]} {confidence:0.2f}"
+#         for _,confidence,class_id,tracker_id
+#         in detections
+#     ]
+#     frame = box_annotator.annotate(scene=frame, detections=detections,labels=labels)
+#     # cv2.imshow("yolov8",frame)
+#     if(cv2.waitKey(30)==27):
+#         cv2.destroyAllWindows()
+#         break
 
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@ https://www.facebook.com/groups/miaigroup/permalink/1594900641281202/
 
 Model v7_ medium-epoch100 (track 8 9 10)\
 Model v8 - only fire
+Run model v8 - pre 16 track 11 - 12 - 13-14
